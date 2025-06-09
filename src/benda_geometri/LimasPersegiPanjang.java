9e8f9ab4cf6816941f09e8798423d90c07c08294
HafzFath <150516305+HafzFath@users.noreply.github.com>
2025-06-09 10:01:36 +0700

    Update LimasPersegiPanjang.java

--- src/benda_geometri/LimasPersegiPanjang.java
+++ src/benda_geometri/LimasPersegiPanjang.java
@@ -1,53 +1,46 @@
 package benda_geometri;
 
-import java.io.*;
-import java.util.*;
-
-/**
- * 
- */
 public class LimasPersegiPanjang extends PersegiPanjang implements Benda3D {
+    private double tinggi;
+    private double volume;
+    private double luasPermukaan;
 
-    /**
-     * Default constructor
-     */
-    public LimasPersegiPanjang() {
+    public LimasPersegiPanjang(double panjang, double lebar, double tinggi) throws InvalidInputException {
+        super(panjang, lebar);
+        if (tinggi < 0) {
+            throw new InvalidInputException("Tinggi tidak boleh negatif!");
+        }
+        this.tinggi = tinggi;
     }
 
-    /**
-     * 
-     */
-    private double tinggiPrisma;
-
-    /**
-     * @return
-     */
-    public double getTinggiPrisma() {
-        // TODO implement here
-        return 0.0d;
+    @Override
+    public void hitungVolume() {
+        volume = (1.0 / 3.0) * super.luas * tinggi;
     }
 
-    /**
-     * @param tinggiPrisma
-     */
-    public void setTinggiPrisma(double tinggiPrisma) {
-        // TODO implement here
+    @Override
+    public void hitungLuasPermukaan() {
+        double tinggiSegitigaPanjang = Math.sqrt(Math.pow((lebar / 2), 2) + Math.pow(tinggi, 2));
+        double tinggiSegitigaLebar = Math.sqrt(Math.pow((panjang / 2), 2) + Math.pow(tinggi, 2));
+        double luasSegitigaPanjang = panjang * tinggiSegitigaPanjang / 2;
+        double luasSegitigaLebar = lebar * tinggiSegitigaLebar / 2;
+        luasPermukaan = super.luas + 2 * luasSegitigaPanjang + 2 * luasSegitigaLebar;
     }
 
-    /**
-     * @return
-     */
-    public double hitungVolume() {
-        // TODO implement Benda3D.hitungVolume() here
-        return 0.0d;
+    @Override
+    public void tampilkanInfo() {
+        System.out.println("Bangun\t: Limas Persegi Panjang");
+        System.out.println("Panjang Alas\t: " + panjang);
+        System.out.println("Lebar Alas\t: " + lebar);
+        System.out.println("Tinggi\t: " + tinggi);
+        System.out.println("Volume\t: " + volume);
+        System.out.println("Luas Permukaan\t: " + luasPermukaan);
     }
 
-    /**
-     * @return
-     */
-    public double hitungLuasPermukaan() {
-        // TODO implement Benda3D.hitungLuasPermukaan() here
-        return 0.0d;
+    @Override
+    public void run() {
+        hitungVolume();
+        hitungLuasPermukaan();
+        tampilkanInfo();
     }
-
 }
