b2111e9047021a42d5eedea4a26cf9263b97cf10
HafzFath <150516305+HafzFath@users.noreply.github.com>
2025-06-09 13:58:02 +0700

    Update LimasTrapesium.java

--- src/benda_geometri/LimasTrapesium.java
+++ src/benda_geometri/LimasTrapesium.java
@@ -11,15 +11,20 @@ public class LimasTrapesium extends Trapesium implements Benda3D {
             throw new InvalidInputException("Tinggi tidak boleh negatif!");
         }
         this.tinggi = tinggi;
+        hitungVolume();
+        hitungLuasPermukaan();
     }
 
     @Override
     public void hitungVolume() {
+        super.hitungLuas();
         volume = (1.0 / 3.0) * super.luas * tinggi;
     }
 
     @Override
     public void hitungLuasPermukaan() {
+        super.hitungLuas();
+        super.hitungKeliling();
         double tinggiSegitigaAtas = Math.sqrt(Math.pow((sisiBawah - sisiAtas) / 2, 2) + Math.pow(tinggi, 2));
         double tinggiSegitigaKiri = Math.sqrt(Math.pow((sisiKanan - sisiKiri) / 2, 2) + Math.pow(tinggi, 2));
         double luasSegitigaAtas = sisiAtas * tinggiSegitigaAtas / 2;
