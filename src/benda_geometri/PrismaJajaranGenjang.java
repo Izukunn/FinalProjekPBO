4ae10374db3e730f26c53d98d812b85b3ff08d33
reiza048 <55349816+reiza048@users.noreply.github.com>
2025-06-10 20:10:20 +0700

    Update PrismaJajaranGenjang.java

--- src/benda_geometri/PrismaJajaranGenjang.java
+++ src/benda_geometri/PrismaJajaranGenjang.java
@@ -1,53 +1,54 @@
 package benda_geometri;
 
-import java.io.*;
-import java.util.*;
-
-/**
- * 
- */
 public class PrismaJajaranGenjang extends JajaranGenjang implements Benda3D {
 
-    /**
-     * Default constructor
-     */
-    public PrismaJajaranGenjang() {
-    }
-
-    /**
-     * 
-     */
     private double tinggiPrisma;
+    private double volume;
+    private double luasPermukaan;
 
-    /**
-     * @return
-     */
-    public double getTinggiPrisma() {
-        // TODO implement here
-        return 0.0d;
+    public PrismaJajaranGenjang() {
+        super(6, 4, 5);
+        this.tinggiPrisma = 12;
+        hitungVolume();
+        hitungLuasPermukaan();
     }
 
-    /**
-     * @param tinggiPrisma
-     */
-    public void setTinggiPrisma(double tinggiPrisma) {
-        // TODO implement here
+    public PrismaJajaranGenjang(double alas, double tinggiJajar, double sisiMiring, double tinggiPrisma) throws InvalidInputException {
+        super(alas, tinggiJajar, sisiMiring);
+        if (tinggiPrisma < 0) {
+            throw new InvalidInputException("Tinggi prisma tidak boleh negatif!");
+        }
+        this.tinggiPrisma = tinggiPrisma;
+        hitungVolume();
+        hitungLuasPermukaan();
     }
 
-    /**
-     * @return
-     */
-    public double hitungVolume() {
-        // TODO implement Benda3D.hitungVolume() here
-        return 0.0d;
+    @Override
+    public void hitungVolume() {
+        volume = super.hitungLuasReturn() * tinggiPrisma;
     }
 
-    /**
-     * @return
-     */
-    public double hitungLuasPermukaan() {
-        // TODO implement Benda3D.hitungLuasPermukaan() here
-        return 0.0d;
+    @Override
+    public void hitungLuasPermukaan() {
+        double kelilingAlas = super.hitungKelilingReturn();
+        luasPermukaan = (2 * super.hitungLuasReturn()) + (kelilingAlas * tinggiPrisma);
     }
 
+    @Override
+    public String tampilkanInfo() {
+        return "=== PRISMA JAJARAN GENJANG ===\n"
+                + "Alas\t: " + getAlas() + "\n"
+                + "Tinggi Jajargenjang: " + getTinggi() + "\n"
+                + "Sisi Miring\t: " + getSisiMiring() + "\n"
+                + "Tinggi Prisma\t: " + tinggiPrisma + "\n"
+                + "Volume\t: " + volume + "\n"
+                + "Luas Permukaan: " + luasPermukaan;
+    }
+
+    @Override
+    public void run() {
+        hitungVolume();
+        hitungLuasPermukaan();
+        tampilkanInfo();
+    }
 }
