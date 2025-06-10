711ee205de7a8677f30426c12dad70e9f8dad6a2
reiza048 <55349816+reiza048@users.noreply.github.com>
2025-06-10 20:10:42 +0700

    Update LimasSegitiga.java

--- src/benda_geometri/LimasSegitiga.java
+++ src/benda_geometri/LimasSegitiga.java
@@ -1,53 +1,56 @@
 package benda_geometri;
 
-import java.io.*;
-import java.util.*;
-
-/**
- * 
- */
 public class LimasSegitiga extends Segitiga implements Benda3D {
 
-    /**
-     * Default constructor
-     */
+    private double tinggiLimas;
+    private double volume;
+    private double luasPermukaan;
+
     public LimasSegitiga() {
+        super(6, 4);
+        this.tinggiLimas = 10;
+        hitungVolume();
+        hitungLuasPermukaan();
     }
 
-    /**
-     * 
-     */
-    private double tinggiPrisma;
-
-    /**
-     * @return
-     */
-    public double getTinggiPrisma() {
-        // TODO implement here
-        return 0.0d;
+    public LimasSegitiga(double alas, double tinggiSegitiga, double tinggiLimas) throws InvalidInputException {
+        super(alas, tinggiSegitiga);
+        if (tinggiLimas < 0) {
+            throw new InvalidInputException("Tinggi limas tidak boleh negatif!");
+        }
+        this.tinggiLimas = tinggiLimas;
+        hitungVolume();
+        hitungLuasPermukaan();
     }
 
-    /**
-     * @param tinggiPrisma
-     */
-    public void setTinggiPrisma(double tinggiPrisma) {
-        // TODO implement here
+    @Override
+    public void hitungVolume() {
+        volume = (1.0 / 3.0) * super.hitungLuasReturn() * tinggiLimas;
     }
 
-    /**
-     * @return
-     */
-    public double hitungVolume() {
-        // TODO implement Benda3D.hitungVolume() here
-        return 0.0d;
+    @Override
+    public void hitungLuasPermukaan() {
+        double kelilingAlas = super.hitungKelilingReturn();
+        // Luas segitiga sisi samping = 1/2 * (kelilingAlas / 3) * tinggiSisi
+        double tinggiSisi = Math.sqrt(Math.pow(tinggiLimas, 2) + Math.pow((super.getAlas() / 2), 2));
+        double luasSisi = 0.5 * (super.getAlas() / 2) * tinggiSisi;
+        luasPermukaan = super.hitungLuasReturn() + (3 * luasSisi);
     }
 
-    /**
-     * @return
-     */
-    public double hitungLuasPermukaan() {
-        // TODO implement Benda3D.hitungLuasPermukaan() here
-        return 0.0d;
+    @Override
+    public String tampilkanInfo() {
+        return "=== LIMAS SEGITIGA ===\n"
+                + "Alas\t: " + getAlas() + "\n"
+                + "Tinggi Segitiga: " + getTinggi() + "\n"
+                + "Tinggi Limas\t: " + tinggiLimas + "\n"
+                + "Volume\t: " + volume + "\n"
+                + "Luas Permukaan: " + luasPermukaan;
     }
 
+    @Override
+    public void run() {
+        hitungVolume();
+        hitungLuasPermukaan();
+        tampilkanInfo();
+    }
 }
