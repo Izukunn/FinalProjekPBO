87d084f02f45c45a3e26669f09ae78a3a2b17170
aanorelya <151989987+aanorelya@users.noreply.github.com>
2025-06-10 20:22:19 +0700

    Update BelahKetupat.java

--- src/benda_geometri/BelahKetupat.java
+++ src/benda_geometri/BelahKetupat.java
@@ -1,17 +1,35 @@
 package benda_geometri;
 
-public class BelahKetupat implements Benda2D {
+public class BelahKetupat implements Benda2D, Runnable {
 
     private double diagonal1;
     private double diagonal2;
     private double sisi;
 
+    // Tambah atribut luas & keliling
+    private double luas;
+    private double keliling;
+
+    // Constructor default
+    public BelahKetupat() {
+        // Nilai default 1.0
+        this.diagonal1 = 1.0;
+        this.diagonal2 = 1.0;
+        this.sisi = 1.0;
+        hitungLuas();
+        hitungKeliling();
+    }
+
+    // Constructor dengan parameter
     public BelahKetupat(double diagonal1, double diagonal2, double sisi) throws NegativeInputException {
         setDiagonal1(diagonal1);
         setDiagonal2(diagonal2);
         setSisi(sisi);
+        hitungLuas();
+        hitungKeliling();
     }
 
+    // Getter & Setter
     public double getDiagonal1() {
         return diagonal1;
     }
@@ -45,23 +63,45 @@ public class BelahKetupat implements Benda2D {
         this.sisi = sisi;
     }
 
+    // Getter untuk luas dan keliling
+    public double getLuas() {
+        return luas;
+    }
+
+    public double getKeliling() {
+        return keliling;
+    }
+
+    // Hitung luas (simpan ke atribut luas)
     @Override
     public double hitungLuas() {
-        return (diagonal1 * diagonal2) / 2.0;
+        luas = (diagonal1 * diagonal2) / 2.0;
+        return luas;
     }
 
+    // Hitung keliling (simpan ke atribut keliling)
     @Override
     public double hitungKeliling() {
-        return 4 * sisi;
+        keliling = 4 * sisi;
+        return keliling;
     }
 
+    // Tampilkan info
     @Override
     public void tampilkanInfo() {
-        System.out.println("Bangun\t: Belah Ketupat");
-        System.out.println("Diagonal 1\t: " + getDiagonal1());
-        System.out.println("Diagonal 2\t: " + getDiagonal2());
-        System.out.println("Sisi\t: " + getSisi());
-        System.out.println("Luas\t: " + hitungLuas());
-        System.out.println("Keliling\t: " + hitungKeliling());
+        System.out.println("=== BELAH KETUPAT ===");
+        System.out.println("Diagonal 1\t: " + diagonal1);
+        System.out.println("Diagonal 2\t: " + diagonal2);
+        System.out.println("Sisi\t\t: " + sisi);
+        System.out.println("Luas\t\t: " + luas);
+        System.out.println("Keliling\t: " + keliling);
+    }
+
+    // Runnable (multi-thread)
+    @Override
+    public void run() {
+        hitungLuas();
+        hitungKeliling();
+        tampilkanInfo();
     }
 }
