accd28aafd35089cb783ed68c6e71ed4e14c1344
aanorelya <151989987+aanorelya@users.noreply.github.com>
2025-06-12 07:30:53 +0700

    Update LimasBelahKetupat.java

--- src/benda_geometri/LimasBelahKetupat.java
+++ src/benda_geometri/LimasBelahKetupat.java
@@ -2,63 +2,48 @@ package benda_geometri;
 
 public class LimasBelahKetupat extends BelahKetupat implements Benda3D {
 
-    private double tinggiPrisma;
-    private double volume;
-    private double luasPermukaan;
+    public double tinggiPrisma;
+    public double volume;
+    public double luasPermukaan;
 
-    public LimasBelahKetupat(double diagonal1, double diagonal2, double sisi, double tinggiPrisma) throws InvalidInputException {
+    public LimasBelahKetupat(double diagonal1, double diagonal2, double sisi, double tinggiPrisma) 
+            throws InvalidInputException {
         super(diagonal1, diagonal2, sisi);
-        setTinggiPrisma(tinggiPrisma);
-        hitungVolume();
-        hitungLuasPermukaan();
-    }
-
-    public double getTinggiPrisma() {
-        return tinggiPrisma;
-    }
-
-    public void setTinggiPrisma(double tinggiPrisma) throws InvalidInputException {
         if (tinggiPrisma < 0) {
             throw new InvalidInputException("Tinggi prisma tidak boleh negatif!");
         }
         this.tinggiPrisma = tinggiPrisma;
-    }
-
-    public double getVolume() {
-        return volume;
-    }
-
-    public double getLuasPermukaan() {
-        return luasPermukaan;
+        hitungVolume();
+        hitungLuasPermukaan();
     }
 
     @Override
     public void hitungVolume() {
-        volume = (1.0 / 3) * super.getLuas() * tinggiPrisma;
+        volume = (1.0 / 3) * luas * tinggiPrisma;
     }
 
     @Override
     public void hitungLuasPermukaan() {
-        double tinggiSegitiga = Math.sqrt(Math.pow(tinggiPrisma, 2) + Math.pow(getSisi(), 2) / 4);
-        double luasSelimut = 4 * (0.5 * getSisi() * tinggiSegitiga);
-        luasPermukaan = super.getLuas() + luasSelimut;
+        double tinggiSegitiga = Math.sqrt(Math.pow(tinggiPrisma, 2) + Math.pow(sisi, 2) / 4);
+        double luasSelimut = 4 * (0.5 * sisi * tinggiSegitiga);
+        luasPermukaan = luas + luasSelimut;
     }
 
     @Override
     public String tampilkanInfo() {
-        return "=== LIMAS BELAH KETUPAT ===\n" +
-                "Diagonal 1\t: " + getDiagonal1() + "\n" +
-                "Diagonal 2\t: " + getDiagonal2() + "\n" +
-                "Sisi\t\t: " + getSisi() + "\n" +
-                "Tinggi Prisma\t: " + tinggiPrisma + "\n" +
-                "Volume\t\t: " + volume + "\n" +
-                "Luas Permukaan\t: " + luasPermukaan;
+        return "=== LIMAS BELAH KETUPAT ===\n"
+                + "Diagonal 1\t: " + diagonal1 + "\n"
+                + "Diagonal 2\t: " + diagonal2 + "\n"
+                + "Sisi\t\t: " + sisi + "\n"
+                + "Tinggi Prisma\t: " + tinggiPrisma + "\n"
+                + "Volume\t\t: " + volume + "\n"
+                + "Luas Permukaan\t: " + luasPermukaan;
     }
 
     @Override
     public void run() {
         hitungVolume();
         hitungLuasPermukaan();
-       tampilkanInfo();
+        tampilkanInfo();
     }
 }
