a97a5abdc096235f23faa91e1249826dd3e96838
aanorelya <151989987+aanorelya@users.noreply.github.com>
2025-06-12 07:35:42 +0700

    Update PrismaLayangLayang.java

--- src/benda_geometri/PrismaLayangLayang.java
+++ src/benda_geometri/PrismaLayangLayang.java
@@ -2,82 +2,49 @@ package benda_geometri;
 
 public class PrismaLayangLayang extends LayangLayang implements Benda3D, Runnable {
 
-    private double tinggiPrisma;
-    private double volume;
-    private double luasPermukaan;
+    public double tinggiPrisma;
+    public double volume;
+    public double luasPermukaan;
 
-    // Constructor utama
     public PrismaLayangLayang(double diagonal1, double diagonal2, double sisiPendek, double sisiPanjang, double tinggiPrisma)
             throws InvalidInputException {
         super(diagonal1, diagonal2, sisiPendek, sisiPanjang);
-        setTinggiPrisma(tinggiPrisma);
-        hitungLuas();
-        hitungKeliling();
-        hitungVolume();
-        hitungLuasPermukaan();
-    }
-
-    // Getter dan Setter
-    public double getTinggiPrisma() {
-        return tinggiPrisma;
-    }
-
-    public void setTinggiPrisma(double tinggiPrisma) throws InvalidInputException {
         if (tinggiPrisma < 0) {
             throw new InvalidInputException("Tinggi prisma tidak boleh negatif!");
         }
         this.tinggiPrisma = tinggiPrisma;
-    }
-
-    public double getVolume() {
-        return volume;
-    }
-
-    public double getLuasPermukaan() {
-        return luasPermukaan;
+        hitungVolume();
+        hitungLuasPermukaan();
     }
 
     @Override
     public void hitungVolume() {
-        volume = super.getLuas() * tinggiPrisma;
+        volume = luas * tinggiPrisma;
     }
 
     @Override
     public void hitungLuasPermukaan() {
-        luasPermukaan = (2 * super.getLuas()) + (super.getKeliling() * tinggiPrisma);
-    }
-
-    @Override
-    public void hitungLuas() {
-        super.hitungLuas();
-    }
-
-    @Override
-    public void hitungKeliling() {
-        super.hitungKeliling();
+        luasPermukaan = (2 * luas) + (keliling * tinggiPrisma);
     }
 
     @Override
     public String tampilkanInfo() {
-        return "=== PRISMA LAYANG-LAYANG ===\n" +
-                "Diagonal 1\t: " + getDiagonal1() + "\n" +
-                "Diagonal 2\t: " + getDiagonal2() + "\n" +
-                "Sisi Pendek\t: " + getSisiPendek() + "\n" +
-                "Sisi Panjang\t: " + getSisiPanjang() + "\n" +
-                "Tinggi Prisma\t: " + tinggiPrisma + "\n" +
-                "Luas Alas\t: " + getLuas() + "\n" +
-                "Keliling Alas\t: " + getKeliling() + "\n" +
-                "Volume\t\t: " + volume + "\n" +
-                "Luas Permukaan\t: " + luasPermukaan;
+        return "=== PRISMA LAYANG-LAYANG ===\n"
+                + "Diagonal 1\t: " + diagonal1 + "\n"
+                + "Diagonal 2\t: " + diagonal2 + "\n"
+                + "Sisi Pendek\t: " + sisiPendek + "\n"
+                + "Sisi Panjang\t: " + sisiPanjang + "\n"
+                + "Tinggi Prisma\t: " + tinggiPrisma + "\n"
+                + "Luas Alas\t: " + luas + "\n"
+                + "Keliling Alas\t: " + keliling + "\n"
+                + "Volume\t\t: " + volume + "\n"
+                + "Luas Permukaan\t: " + luasPermukaan;
     }
 
     @Override
     public void run() {
-        hitungLuas();
-        hitungKeliling();
         hitungVolume();
         hitungLuasPermukaan();
         tampilkanInfo();
     }
 }
-
